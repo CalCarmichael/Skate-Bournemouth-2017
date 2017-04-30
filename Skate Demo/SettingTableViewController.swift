11ec3e94b867c1f6232eb67cc98dbcd57ce88465
CalCarmichael <callunclive@hotmail.com>
2017-04-30 16:00:41 +0100

    Edit Profile VC
    Creating the edit profile view controller

--- Skate Demo/SettingTableViewController.swift
+++ Skate Demo/SettingTableViewController.swift
@@ -10,14 +10,49 @@ import UIKit
 
 class SettingTableViewController: UITableViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     
         navigationItem.title = "Edit Profile"
     
-    
+        getCurrentUser()
     
     }
+    
+    //Displaying user information
+    
+    func getCurrentUser() {
+        
+        Api.User.observeCurrentUser { (user) in
+            
+            self.usernameTextField.text = user.username
+            
+            self.emailTextField.text = user.email
+            
+            if let profileUrl = URL(string: user.profileImageUrl!) {
+            
+            self.profileImage.sd_setImage(with: profileUrl)
+                
+            }
+            
+        }
+        
+    }
+    
+    @IBAction func saveButton_TouchUpInside(_ sender: Any) {
+    }
+    
+    @IBAction func logoutButton_TouchUpInside(_ sender: Any) {
+    }
+    
+    
 
 }
